11311fbb0bf2e3fd8b3611c70bc7f13d7112d903
Alexa Carrington <acarring@wellesley.edu>
2021-02-18 11:18:53 -0500

    Added App.css, imported it in js, added styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
 import React, { useState } from 'react';
+import "./App.css";
 
 export default function App() {
   const [state, setState] = useState();
   return (
-    <div>
-      hello world lol
-    </div>
+    <section className="container">
+      <section className="timer">
+        <div>
+          <h2>Countdown Timer</h2>
+          <p>Tick, tock...</p>
+        </div>
+        <div>
+          <section>
+            <p>12</p>
+            <p>Days</p>
+          </section>
+          <span>:</span>
+          <section>
+            <p>12</p>
+            <p>Hours</p>
+          </section>
+          <span>:</span>
+          <section>
+            <p>12</p>
+            <p>Minutes</p>
+          </section>
+          <span>:</span>
+          <section>
+            <p>12</p>
+            <p>Seconds</p>
+          </section>
+        </div>
+      </section>
+    </section>
   )
 }
 
